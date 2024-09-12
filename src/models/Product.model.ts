e1fabf68578e08d19e7ee6767405422056bf524b
EliDev <oscarmateoelias@gmail.com>
2024-09-12 16:49:25 -0600

    Add database configuration and product model

---
+++ src/models/Product.model.ts
@@ -0,0 +1,19 @@
+import { Table, Column, Model, DataType, Default } from 'sequelize-typescript';
+
+@Table({
+    tableName: 'products'
+})
+
+class Product extends Model {
+    @Column({ type: DataType.STRING(100) })
+    name: string
+
+    @Column({ type: DataType.FLOAT(6, 2) })
+    price: number
+
+    @Column({ type: DataType.BOOLEAN })
+    availabilty: boolean
+
+}
+
+export default Product
