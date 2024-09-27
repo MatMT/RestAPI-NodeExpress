e4bcf57275817f6b68133c1398e437967697bf7f
EliDev <oscarmateoelias@gmail.com>
2024-09-27 17:49:48 -0600

    Add: Morgan and CORS dependency. Feat: corsOption defined

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 import colors from 'colors';
+import cors, {CorsOptions} from "cors";
 import swagerUI from 'swagger-ui-express';
 import swaggerSpec, {swagggerUiOptions} from './config/swagger';
 import productsRouter from './router';
 import db from './config/db';
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // Conect to DataBase
 export async function connectDB() {
@@ -19,6 +23,18 @@ export async function connectDB() {
 connectDB();
 
 const server = express();
+
+// Enable connection to the server
+const corsOptions: CorsOptions = {
+    origin: function (origin, callback) {
+        if(origin === process.env.FRONTEND_URL) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+}
+server.use(cors(corsOptions));
 
 server.use(express.json());
 server.use('/api/products', productsRouter);
